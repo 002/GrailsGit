11df414372f0631a4061b3eceab3bd7a4aa733a3
Elmanuchuky <elmanu.chuky@gmail.com>
2018-03-05 11:55:50 -0300

    Hola carola

--- grails-app/domain/grailsgit/Category.groovy
+++ grails-app/domain/grailsgit/Category.groovy
@@ -3,9 +3,14 @@ package grailsgit
 class Category {
 
     String name
+    String urlImage
+    int visits
 
     static belongsTo = [site:Site]
 
     static constraints = {
+        name nullable: false, blank: false
+        urlImage nullable: false, blank: false
+        visits min: 0
     }
 }
