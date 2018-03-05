c81fff6b6dca41f9b523cb23bd1df9d689b1ceed
002 <adrianruh@hotmail.com>
2018-03-05 14:55:25 -0300

    fixes

--- grails-app/init/grailsgit/BootStrap.groovy
+++ grails-app/init/grailsgit/BootStrap.groovy
@@ -9,6 +9,17 @@ class BootStrap {
         user.setName("Juan")
         user.setLastName("Perez")
         user.save()
+
+        Site site = new Site(name: 'Argentina', active: true, siteId: 'MLA')
+        site.save()
+        site = new Site(name: 'Brasil', active: true, siteId: 'MLB')
+        site.save()
+        site = new Site(name: 'Bolivia', active: false, siteId: 'MBO')
+        site.save()
+
+        Category cat = new Category(name: 'Coso', visits: 0 , siteId: 1)
+
+        cat = new Category(name: 'Algo', visits: 0 , siteId: 1)
     }
     def destroy = {
     }
